243b78e19817d8b23784458d59b9a207e5d3f16c
Matt O'Tousa <matt@mattotousa.com>
2020-11-23 15:39:41 -0800

    doors and keys

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isOpen = true;
+    public float duration = 2.0f;
+    public GameObject doorObject;
+    public Vector3 openPosition;
+    public Vector3 closedPosition;
+
+    private void Start()
+    {
+        doorObject.transform.localPosition = isOpen ? closedPosition : openPosition;
+    }
+
+    public void Open()
+    {
+        isOpen = false;
+        StartCoroutine(SlideDoorObjectTo(openPosition));
+    }
+
+    public void Close()
+    {
+        isOpen = true;
+        StartCoroutine(SlideDoorObjectTo(closedPosition));
+    }
+
+    private IEnumerator SlideDoorObjectTo(Vector3 position)
+    {
+        // const float step = 0.01f;
+        float currentTime = 0;
+        Transform doorTransform = doorObject.transform;
+        Vector3 startPosition = doorTransform.localPosition;
+        
+        while (currentTime < duration)
+        {
+            float openPercent = Mathf.InverseLerp(0, duration, currentTime);
+            doorTransform.localPosition = Vector3.Lerp(startPosition, position, openPercent);
+            currentTime += Time.deltaTime;
+            yield return 0;
+        }
+        // yield return new WaitForSeconds(step);
+
+        /*
+        for (float time = 0f; time <= duration; time += step)
+        {
+           // get ratio between ft and duration
+           float openPercent = Mathf.InverseLerp(0, duration, time);
+           doorTransform.localPosition = Vector3.Lerp(startPosition, position, openPercent);
+           yield return new WaitForSeconds(step);
+        }
+        */
+    }
+
+}
---
+++ Assets/Scripts/DoorLock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoorLock : MonoBehaviour
+{
+    public UnityEvent onUnlock;
+    private bool isLocked = true;
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.CompareTag("key"))
+            onUnlock.Invoke();
+    }
+}
