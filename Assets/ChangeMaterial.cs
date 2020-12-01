4a10e357facf54c329cf2c1edb1b73e14bcde440
Matt O'Tousa <matt@mattotousa.com>
2020-11-30 16:40:42 -0800

    locks change color

---
+++ Assets/ChangeMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material material;
+
+    private MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetMaterial()
+    {
+        renderer.material = material;
+    }
+
+    public void SetMaterial(Material mat)
+    {
+        renderer.material = mat;
+    }
+}
